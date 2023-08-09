3ce99e436ba6fd13a403ed748d28f899c1d70f41
조경제 <jskytorod@gmail.com>
2023-08-09 19:03:21 +0900

    0809

---
+++ src/main/java/com/example/backend/Service/FoodInfoService.java
@@ -0,0 +1,23 @@
+package com.example.backend.Service;
+
+import com.example.backend.Entity.FoodInfo;
+import com.example.backend.Repository.FoodInfoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FoodInfoService {
+
+    @Autowired
+    private FoodInfoRepository foodInfoRepository;
+
+    public List<FoodInfo> findAll(){
+
+        return foodInfoRepository.findAll();
+    }
+    public void save(FoodInfo foodInfo){
+        foodInfoRepository.save(foodInfo);
+    }
+}
