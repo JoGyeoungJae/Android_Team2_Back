255d10b761876c6a3317f78feb81b1147b397631
eunseongzzangzzangman <8ohowb@naver.com>
2023-08-11 16:18:22 +0900

    230811 hotfix1 보냄 백엔드

---
+++ src/main/java/com/example/backend/Controller/CommentController.java
@@ -0,0 +1,69 @@
+package com.example.backend.Controller;
+
+import com.example.backend.Entity.Comment;
+import com.example.backend.Service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CommentController {
+
+//    @PostMapping("/signup")
+//    public String signUp(@RequestBody SignupRequest request) {
+//        String id = request.getId();
+//        String password = request.getPassword();
+//        String email = request.getEmail();
+//        String phone = request.getPhone();
+//
+//        // 전송된 데이터를 로그에 출력하거나 원하는 처리를 수행합니다.
+//        System.out.println("Received data:");
+//        System.out.println("Id: " + id);
+//        System.out.println("Password: " + password);
+//        System.out.println("Email: " + email);
+//        System.out.println("Phone: " + phone);
+//
+//        return "Data received successfully";
+//    }
+
+
+//    @RequestMapping("/hello")
+//    public String hello() {
+//        System.out.println("Received data:");
+//        return "Hello REST!";
+//    }
+
+//    @PostMapping("/signup")
+
+
+    private final CommentService commentService;
+
+    @Autowired
+    public CommentController(CommentService commentService) {this.commentService = commentService;}
+
+    @RequestMapping("/comments")
+    public String postComment( @RequestBody Comment comment) {
+
+        String cmt = comment.getCmt();
+        String cmtTime = comment.getCmtTime();
+
+        // 전송된 데이터를 로그에 출력하거나 원하는 처리를 수행합니다.
+        System.out.println("Received data:");
+        System.out.println("cmt: " + cmt);
+        System.out.println("cmtTime" + cmtTime);
+
+        //db저장
+        Comment comment1 = new Comment();
+        comment1.setCmt(cmt);
+        comment1.setCmtTime(cmtTime);
+
+
+        commentService.saveComment(comment1);
+
+
+        return "Data received successfully";
+    }
+
+
+}
