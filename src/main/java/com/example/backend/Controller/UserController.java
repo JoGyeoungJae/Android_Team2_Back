a838d63570345cbebaa7aa7ceb2dd39513612ddb
leeos20k <leeos20k@naver.com>
2023-08-11 09:27:20 +0900

    연동테스트222

--- src/main/java/com/example/backend/Controller/UserController.java
+++ src/main/java/com/example/backend/Controller/UserController.java
@@ -43,7 +43,7 @@ public class UserController {
         newUser.setUname(uname);
         newUser.setUnickname(unickname);
         newUser.setUimg(uimg);
-        //연동테스트
+        //연동테스트2222
 
         userService.saveUser(newUser);
 
