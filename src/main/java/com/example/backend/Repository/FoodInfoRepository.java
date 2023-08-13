49bad98dc671dc012a181e31cf7067c83fe94f4d
조경제 <jskytorod@gmail.com>
2023-08-13 21:01:48 +0900

    0813 joj 커밋11111111

--- src/main/java/com/example/backend/Repository/FoodInfoRepository.java
+++ src/main/java/com/example/backend/Repository/FoodInfoRepository.java
@@ -3,5 +3,10 @@ package com.example.backend.Repository;
 import com.example.backend.Entity.FoodInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FoodInfoRepository extends JpaRepository<FoodInfo,Long> {
+    List<FoodInfo> findAllByCid(Long cid);
+
+    List<FoodInfo> findTop5ByOrderByRstaravgDesc();
 }
